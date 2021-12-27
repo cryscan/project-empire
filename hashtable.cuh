7382be1bcb065f5d2459e83b198f6b52841bd84b
Evan WANG <wyq@umich.edu>
2021-12-27 16:55:46 -0600

    hashtable insert deadlock

--- hashtable.cuh
+++ hashtable.cuh
@@ -1,27 +1,87 @@
-#pragma once
-#include <vector>
-struct KeyValue
-{
-    uint32_t key;
-    uint32_t value;
+#ifndef PROJECT_EMPIRE_HASHTABLE_CUH
+#define PROJECT_EMPIRE_HASHTABLE_CUH
+
+// #include "hashtable.cu"
+#include "common.h"
+#include <cstdlib>
+#include <device_atomic_functions.h>
+
+template <typename Node, typename Value>
+class Hashtable {
+public:
+	using StatePtr = Arc<State<Node, Value>>;
+
+	explicit Hashtable(size_t capacity) :
+		capacity(capacity) {
+		HANDLE_RESULT(cudaMalloc(&states, capacity * sizeof(StatePtr)))
+		HANDLE_RESULT(cudaMemset(states, 0, capacity * sizeof(StatePtr)))
+		HANDLE_RESULT(cudaMalloc(&locks, capacity * sizeof(int)))
+		HANDLE_RESULT(cudaMemset(locks, 0, capacity * sizeof(int)))
+	}
+
+	~Hashtable() {
+		HANDLE_RESULT(cudaFree(states))
+		HANDLE_RESULT(cudaFree(locks))
+	}
+
+	__device__ void insert(Node& key, StatePtr& state_ptr) {
+		// step1: hash
+		size_t slot = hash(key);
+		// for (int i = 0; i < 32; i++) {
+			// if (threadIdx.x % 32 == i) {
+		// if old is 1, keep going; if old is 0, quit (ie execute)
+		while (atomicExch(&locks[slot], 1) == 1);
+		// do here
+		atomicExch(&locks[slot], 0);
+			//critical section
+		// states[slot] = state_ptr;
+				
+			// }
+		// }
+		return;
+		// step2: copy Value to hashtable & make it atomic
+		// StatePtr* target = (states + slot);
+		// atomicExch(target, state_ptr);
+		// state_ptr = *states;
+		// StatePtr test = state_ptr;
+		// atomicExch(&states[slot], state_ptr);
+	}
+	__device__ bool find(const Node& key, StatePtr& output) {
+		// step1: hash
+		size_t slot = hash(key);
+
+		// step2: check initial & check node == key
+		// StatePtr* target = states + slot;
+		
+		if ((states+slot) == nullptr) return false;
+		if (!(states[slot]->node == key)) return false;
+
+		// step3: if found, update output
+		output = states[slot];
+		
+		return true;
+	}
+	
+
+private:
+	size_t capacity;
+	StatePtr* states;
+	int* locks;
+
+	// template <typename T>
+	__device__ size_t hash(Node node) {
+		// if (Node == uint32_t) {
+			//node ^= node >> 16;
+			//node *= 0x85ebca6b;
+			//node ^= node >> 13;
+			//node *= 0xc2b2ae35;
+			//node ^= node >> 16;
+		// }
+		// return node & (capacity - 1);
+		return 0;
+
+	}
 };
 
-const uint32_t kHashTableCapacity = 128 * 1024 * 1024;
 
-const uint32_t kNumKeyValues = kHashTableCapacity / 2;
-
-const uint32_t kEmpty = 0xffffffff;
-
-KeyValue* create_hashtable();
-
-void insert_hashtable(KeyValue* hashtable, const KeyValue* kvs, uint32_t num_kvs);
-
-void lookup_hashtable(KeyValue* hashtable, KeyValue* kvs, uint32_t num_kvs);
-
-void delete_hashtable(KeyValue* hashtable, const KeyValue* kvs, uint32_t num_kvs);
-
-std::vector<KeyValue> iterate_hashtable(KeyValue* hashtable);
-
-void destroy_hashtable(KeyValue* hashtable);
-
-void lookup_compare_hashtable(KeyValue* hashtable, KeyValue* kvs, uint32_t num_kvs);
+#endif // !PROJECT_EMPIRE_HASHTABLE_CUH
--- main.cu
+++ main.cu
@@ -1,12 +1,14 @@
 #include <vector>
 #include <iostream>
 #include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 #include "common.h"
 #include "heap.h"
 #include "hashtable.cuh"
 
 using HeapType = Heap<uint64_t, unsigned>;
 using StateType = State<uint64_t, unsigned>;
+using TableType = Hashtable<uint64_t, unsigned>;
 
 __global__ void test(HeapType* heap_dev, unsigned* buf) {
     auto s1 = make_arc<StateType>();
@@ -34,8 +36,29 @@ __global__ void test(HeapType* heap_dev, unsigned* buf) {
     buf[4] = heap_dev->pop()->f;
 }
 
+
+__global__ void test_hash(TableType* table_dev, unsigned* buf_dev, bool* bool_dev) {
+    size_t gindex = threadIdx.x + blockIdx.x * blockDim.x;
+    buf_dev[gindex] = gindex;
+    // if (gindex > 0) return;
+    uint64_t v1 = gindex;
+    Arc<StateType> s1 = make_arc<StateType>();
+    s1->node = v1;
+    Arc<StateType> out;
+    if (gindex == 0 || gindex == 32)  table_dev->insert(v1, s1);
+    // cudaDeviceSynchronize();
+    // bool_dev[gindex] = 1;
+    // table_dev->find(10, out);
+    // bool_dev[gindex] = table_dev->find(v1, out);
+    //if (bool_dev[gindex])
+    //{
+    //    buf_dev[gindex] = out->node;
+    //}
+
+}
+
 int main(int argc, char** argv) {
-    HeapType heap(1024);
+    /*HeapType heap(1024);
 
     HeapType* heap_dev;
     HANDLE_RESULT(cudaMalloc(&heap_dev, sizeof(HeapType)))
@@ -44,30 +67,42 @@ int main(int argc, char** argv) {
     unsigned* buf_dev;
     HANDLE_RESULT(cudaMalloc(&buf_dev, 5 * sizeof(unsigned)))
 
-    test<<<1, 1>>>(heap_dev, buf_dev);
+    test<< <1, 1> >>(heap_dev, buf_dev);
 
     unsigned buf[5];
-    HANDLE_RESULT(cudaMemcpy(buf, buf_dev, 5 * sizeof(unsigned), cudaMemcpyDeviceToHost))
+    HANDLE_RESULT(cudaMemcpy(buf, buf_dev, 5 * sizeof(unsigned), cudaMemcpyDeviceToHost))*/
+    constexpr size_t thread_count = 64;
 
+    TableType table(thread_count);
+    
+    TableType* table_dev;
+    HANDLE_RESULT(cudaMalloc(&table_dev, sizeof(TableType)))
+    HANDLE_RESULT(cudaMemcpy(table_dev, &table, sizeof(TableType), cudaMemcpyHostToDevice))
 
-    int num_of_elements = 1000;
-    std::vector<KeyValue> insert_kvs;
-    insert_kvs.reserve(num_of_elements);
-    for (uint32_t i = 0; i < num_of_elements; i++)
+    unsigned* buf_dev;
+    HANDLE_RESULT(cudaMalloc(&buf_dev, thread_count * sizeof(unsigned)));
+
+    bool* bool_dev;
+    HANDLE_RESULT(cudaMalloc(&bool_dev, thread_count * sizeof(bool)));
+
+    test_hash << <1, thread_count>> > (table_dev, buf_dev, bool_dev);
+
+    unsigned buf[thread_count];
+    HANDLE_RESULT(cudaMemcpy(buf, buf_dev, thread_count * sizeof(unsigned), cudaMemcpyDeviceToHost))
+
+    bool bool_buf[thread_count];
+    HANDLE_RESULT(cudaMemcpy(bool_buf, bool_dev, thread_count * sizeof(bool), cudaMemcpyDeviceToHost))
+
+    std::cout << "elements: \n";
+    for (auto element : buf)
     {
-        insert_kvs.push_back(KeyValue{ i, i * 2 });
+        std::cout << element << '\n';
     }
-    // create hashtable
-    auto test = create_hashtable();
-    // insert into hashtable
-    insert_hashtable(test, insert_kvs.data(), insert_kvs.size());
-    // iterate through hashtable
-    std::vector<KeyValue> output = iterate_hashtable(test);
-    for (int i = 0; i < num_of_elements; i++)
+    
+    std::cout << "finds: \n";
+    for (auto element : bool_buf)
     {
-        std::cout << output[i].key << " " << output[i].value << "\n";
+        std::cout << element << '\n';
     }
-    destroy_hashtable(test);
-
     return 0;
 }
